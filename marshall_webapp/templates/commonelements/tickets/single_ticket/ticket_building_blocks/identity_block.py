d1b7bef351385c51ea3add72454206d4f89d7d26
thespacedoctor <nothingbutdave@gmail.com>
2016-02-14 20:29:39 +0000

    fixed bug: aka survey credientials not getting passed to URL

--- marshall_webapp/templates/commonelements/tickets/single_ticket/ticket_building_blocks/identity_block.py
+++ marshall_webapp/templates/commonelements/tickets/single_ticket/ticket_building_blocks/identity_block.py
@@ -281,6 +281,37 @@ def identity_block(
     # AKA NAMES
     rows = []
     for item in objectAkas:
+        surveyObjectUrl = item["surveyObjectUrl"]
+        if surveyObjectUrl and "portal.nersc.gov/" in surveyObjectUrl:
+            user = request.registry.settings["credentials"]["lsq"]["username"]
+            pwd = request.registry.settings["credentials"]["lsq"]["password"]
+            surveyObjectUrl = surveyObjectUrl.replace(
+                "portal.", "%(user)s:%(pwd)s@portal." % locals())
+
+        if surveyObjectUrl and ("ps13pi" in surveyObjectUrl):
+            user = request.registry.settings[
+                "credentials"]["ps1-3pi"]["username"]
+            pwd = request.registry.settings[
+                "credentials"]["ps1-3pi"]["password"]
+            surveyObjectUrl = surveyObjectUrl.replace(
+                "star.", "%(user)s:%(pwd)s@star." % locals())
+
+        if surveyObjectUrl and ("ps1fgss" in surveyObjectUrl):
+            user = request.registry.settings[
+                "credentials"]["ps1-fgss"]["username"]
+            pwd = request.registry.settings[
+                "credentials"]["ps1-fgss"]["password"]
+            surveyObjectUrl = surveyObjectUrl.replace(
+                "star.", "%(user)s:%(pwd)s@star." % locals())
+
+        if surveyObjectUrl and ("ps1gw" in surveyObjectUrl):
+            user = request.registry.settings[
+                "credentials"]["ps1-gw"]["username"]
+            pwd = request.registry.settings[
+                "credentials"]["ps1-gw"]["password"]
+            surveyObjectUrl = surveyObjectUrl.replace(
+                "star.", "%(user)s:%(pwd)s@star." % locals())
+        item["surveyObjectUrl"] = surveyObjectUrl
         if item["transientBucketId"] == discoveryDataDictionary["transientBucketId"] and item["name"] != discoveryDataDictionary["masterName"]:
             rows.append(item)
 
@@ -297,7 +328,7 @@ def identity_block(
 
         aka = khufu.a(
             content=aka,
-            href=row["surveyObjectUrl"],
+            href=surveyObjectUrl,
             openInNewTab=True
         )
 
