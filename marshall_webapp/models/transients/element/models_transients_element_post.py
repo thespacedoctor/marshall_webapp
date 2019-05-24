bee4d81ab7018d1d8ff7b3561ee29b95b2a78daa
thespacedoctor <nothingbutdave@gmail.com>
2019-05-24 15:27:58 +0100

    bug fix - classification form

--- marshall_webapp/models/transients/element/models_transients_element_post.py
+++ marshall_webapp/models/transients/element/models_transients_element_post.py
@@ -107,7 +107,7 @@ class models_transients_element_post():
         # first select out a row from the transientBucket as a template for the
         # classification
         sqlQuery = """
-            select p.classifiedFlag, t.raDeg, t.decDeg, t.name, t.htm20ID, t.htm16ID, t.cx, t.cy, t.cz from transientBucket t, pesstoObjects p where replacedByRowId = 0 and t.transientBucketId = %(transientBucketId)s and t.transientBucketId=p.transientBucketId limit 1
+            select p.classifiedFlag, t.raDeg, t.decDeg, t.name, t.htm20ID, t.htm16ID from transientBucket t, pesstoObjects p where replacedByRowId = 0 and t.transientBucketId = %(transientBucketId)s and t.transientBucketId=p.transientBucketId limit 1
         """ % locals()
         rowsTmp = self.request.db.execute(sqlQuery).fetchall()
         rows = []
@@ -152,7 +152,7 @@ class models_transients_element_post():
         duplicate = False
         try:
             sqlQuery = """
-                INSERT INTO transientBucket (raDeg, decDeg, name, htm16ID, cx, cy, cz, transientBucketId, observationDate, observationMjd, survey, spectralType, transientRedshift, dateCreated, dateLastModified, classificationWRTMax, classificationPhase, reducer) VALUES(%(raDeg)s, %(decDeg)s, "%(name)s", %(htm16ID)s, %(cx)s, %(cy)s, %(cz)s, %(transientBucketId)s, "%(clsObsdate)s", %(obsMjd)s, "%(clsSource)s", "%(clsType)s", %(clsRedshift)s, "%(now)s", "%(now)s", "%(clsClassificationWRTMax)s", %(clsClassificationPhase)s, "%(username)s");
+                INSERT INTO transientBucket (raDeg, decDeg, name, htm16ID, transientBucketId, observationDate, observationMjd, survey, spectralType, transientRedshift, dateCreated, dateLastModified, classificationWRTMax, classificationPhase, reducer) VALUES(%(raDeg)s, %(decDeg)s, "%(name)s", %(htm16ID)s, %(transientBucketId)s, "%(clsObsdate)s", %(obsMjd)s, "%(clsSource)s", "%(clsType)s", %(clsRedshift)s, "%(now)s", "%(now)s", "%(clsClassificationWRTMax)s", %(clsClassificationPhase)s, "%(username)s");
             """ % locals()
             self.request.db.execute(sqlQuery)
             self.request.db.commit()
@@ -170,7 +170,7 @@ class models_transients_element_post():
             primaryKeyId = objectData[0]["primaryKeyId"]
 
             sqlQuery = """
-                INSERT IGNORE INTO transientBucket (raDeg, decDeg, name, htm16ID, cx, cy, cz, transientBucketId, observationDate, observationMjd, survey, spectralType, transientRedshift, dateCreated, dateLastModified, classificationWRTMax, classificationPhase, reducer, replacedByRowId) VALUES(%(raDeg)s, %(decDeg)s, "%(name)s", %(htm16ID)s, %(cx)s, %(cy)s, %(cz)s, %(transientBucketId)s, "%(clsObsdate)s", %(obsMjd)s, "%(clsSource)s", "%(clsType)s", %(clsRedshift)s, "%(now)s", "%(now)s", "%(clsClassificationWRTMax)s", %(clsClassificationPhase)s, "%(username)s", %(primaryKeyId)s);
+                INSERT IGNORE INTO transientBucket (raDeg, decDeg, name, htm16ID, transientBucketId, observationDate, observationMjd, survey, spectralType, transientRedshift, dateCreated, dateLastModified, classificationWRTMax, classificationPhase, reducer, replacedByRowId) VALUES(%(raDeg)s, %(decDeg)s, "%(name)s", %(htm16ID)s, %(transientBucketId)s, "%(clsObsdate)s", %(obsMjd)s, "%(clsSource)s", "%(clsType)s", %(clsRedshift)s, "%(now)s", "%(now)s", "%(clsClassificationWRTMax)s", %(clsClassificationPhase)s, "%(username)s", %(primaryKeyId)s);
             """ % locals()
             self.log.debug('sqlQuery: %(sqlQuery)s' % locals())
             self.request.db.execute(sqlQuery)
