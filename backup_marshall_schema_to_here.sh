0cd9212a9a06a7a8e20de35b5814fb3632592c18
Dave <d.r.young@qub.ac.uk>
2019-10-03 17:57:48 +0100

    updating the backup schema script

--- backup_marshall_schema_to_here.sh
+++ backup_marshall_schema_to_here.sh
@@ -1,6 +1,4 @@
 # USE THJS SCRIPT TO BACKUP THE MARSHALL SCHEME FROM THE DATABASE CURRENTLY LIVING ON MY MAC
-
-cd ~/git_repos/marshall_webapp/marshall_webapp/tests/input
 echo "please give password for the marshall user"
 /usr/local/mysql/bin/mysql -u marshall --password=mar5ha11 marshall -e "update meta_workflow_lists_counts set count = 0"
 /usr/local/mysql/bin/mysqldump -u marshall --password=mar5ha11 --no-data --routines marshall > marshall_schema.sql
@@ -9,6 +7,8 @@ echo "please give password for the marshall user again"
 perl -p -i.bak -e "s/DEFINER=\`\w.*?\`@\`.*?\`//g" marshall_schema.sql
 perl -p -i.bak -e "s/ALTER DATABASE .*?CHARACTER.*?;//g" marshall_schema.sql
 perl -p -i.bak -e "s/AUTO_INCREMENT=\d*//g"  marshall_schema.sql
+
+cp marshall_schema.sql ~/git_repos/marshall_webapp/marshall_webapp/tests/input
 
 echo "want to clear out unit_tests marshall database? [y|n]:"
 
