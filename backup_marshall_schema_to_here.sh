e5525f55483ec65f9fd5cd2516061de917c0db67
Dave <d.r.young@qub.ac.uk>
2020-12-09 20:57:19 +0000

    x

--- backup_marshall_schema_to_here.sh
+++ backup_marshall_schema_to_here.sh
@@ -1,10 +1,7 @@
 # USE THJS SCRIPT TO BACKUP THE MARSHALL SCHEME FROM THE DATABASE CURRENTLY LIVING ON MY MAC
 cd ~/git_repos/_webapps_/marshall_webapp/marshall_webapp/tests/input
-/usr/local/mysql/bin/mysql -u marshall --password=mar5ha11 marshall -e "update meta_workflow_lists_counts set count = 0"
-echo "please give password for the marshall user"
-/usr/local/mysql/bin/mysqldump -u marshall --password=mar5ha11 --no-data --routines marshall > marshall_schema.sql
-echo "please give password for the marshall user again"
-/usr/local/mysql/bin/mysqldump -u marshall --password=mar5ha11  marshall meta_workflow_lists_counts webapp_users marshall_fs_column_map stats_ssdr1_overview  stats_ssdr2_overview stats_ssdr3_overview >> marshall_schema.sql
+mysqldump -u marshall --password=mar5ha11 -h10.131.21.162 --port=9001 --no-data --routines marshall > marshall_schema.sql
+mysqldump -u marshall --password=mar5ha11 -h10.131.21.162 --port=9001 marshall meta_workflow_lists_counts webapp_users stats_ssdr1_overview  stats_ssdr2_overview stats_ssdr3_overview marshall_fs_column_map >> marshall_schema.sql
 perl -p -i.bak -e "s/DEFINER=\`\w.*?\`@\`.*?\`//g" marshall_schema.sql
 perl -p -i.bak -e "s/ALTER DATABASE .*?CHARACTER.*?;//g" marshall_schema.sql
 perl -p -i.bak -e "s/AUTO_INCREMENT=\d*//g"  marshall_schema.sql
@@ -20,3 +17,5 @@ then
 fi
 
 mysql -u utuser --password=utpass unit_tests < marshall_schema.sql
+
+
