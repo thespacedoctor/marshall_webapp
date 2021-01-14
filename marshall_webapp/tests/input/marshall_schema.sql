6d109d4c65f1d54456380b6ad4ce0c6e2f895558
thespacedoctor <d.r.young@qub.ac.uk>
2021-01-14 13:09:38 +0000

    updating schema

--- marshall_webapp/tests/input/marshall_schema.sql
+++ marshall_webapp/tests/input/marshall_schema.sql
@@ -5257,7 +5257,7 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-14 13:08:53
+-- Dump completed on 2021-01-14 13:09:38
 -- MySQL dump 10.17  Distrib 10.3.25-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: 10.131.21.162    Database: marshall
@@ -5445,4 +5445,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-14 13:08:53
+-- Dump completed on 2021-01-14 13:09:38
