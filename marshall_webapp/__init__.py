315a946ea789a7ff225b556b8fa8012ef1ee9551
thespacedoctor <d.r.young@qub.ac.uk>
2021-11-24 12:50:03 +0000

    added an internal advanced settings file that can be overridden in marshall.yaml config

--- marshall_webapp/__init__.py
+++ marshall_webapp/__init__.py
@@ -8,7 +8,8 @@ from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.settings import aslist
 from os.path import expanduser
-
+import inspect
+import os
 # ALLOW DEBUG TOOLBAR OVER HTTPS
 from pyramid.url import URLMethodsMixin
 URLMethodsMixin.static_url_org = URLMethodsMixin.static_url  # backup of original
@@ -51,10 +52,32 @@ def main(global_config, **settings):
     config.registry.dbmaker = sessionmaker(bind=engine)
     config.add_request_method(db, reify=True)
 
-    # Add settings from YAML file
-
+    # ADD SETTINGS FROM YAML FILE
     myWebapp = AssetResolver("marshall_webapp")
     theseSettings = config.get_settings()
+
+    import yaml
+    # COLLECT ADVANCED SETTINGS IF AVAILABLE
+    parentDirectory = os.path.dirname(__file__)
+    advs = parentDirectory + "/advanced_settings.yaml"
+    level = 0
+    exists = False
+    count = 1
+    while not exists and len(advs) and count < 10:
+        count += 1
+        level -= 1
+        exists = os.path.exists(advs)
+        if not exists:
+            advs = "/".join(parentDirectory.split("/")
+                            [:level]) + "/advanced_settings.yaml"
+            print(advs)
+    if not exists:
+        advs = {}
+    else:
+        with open(advs, 'r') as stream:
+            advs = yaml.safe_load(stream)
+    config.add_settings(advs)
+
     settingsPath = theseSettings["settingsFile"]
     if settingsPath[0] == "~":
         from os.path import expanduser
